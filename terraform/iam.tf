18648369a16a72746512cfb9f73d34ae8e261568
Jim Lamb <jal25@Jims-MBP.cust.communityfibre.co.uk>
2023-06-03 08:19:50 +0100

    Format changes

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,7 +4,7 @@ resource "aws_iam_policy" "hulahoop_protected_server" {
   name        = "hulahoop_protected_server"
   path        = "/"
   description = "Hulahoop policy to be attached to user for protected server"
-  policy      = "${file("policies/hulahoop_protected_server.json")}"
+  policy      = file("policies/hulahoop_protected_server.json")
 }
 
 resource "aws_iam_user" "hulahoop_protected_server" {
@@ -22,7 +22,13 @@ resource "aws_iam_policy" "hulahoop_jump_server" {
   name        = "hulahoop_jump_server"
   path        = "/"
   description = "Hulahoop policy to be attached to role for hulahoop jump server"
-  policy      = "${data.template_file.hulahoop_jump_server_policy.rendered}"
+  policy = templatefile("policies/hulahoop_jump_server.tpl",
+    {
+      region            = var.aws_region,
+      account_id        = data.aws_caller_identity.current.account_id,
+      security_group_id = aws_security_group.hulahoop_jump_server.id
+    }
+  )
 }
 
 resource "aws_iam_role" "hulahoop_jump_server" {
@@ -56,23 +62,20 @@ resource "aws_iam_instance_profile" "hulahoop_jump_server" {
   role = aws_iam_role.hulahoop_jump_server.name
 }
 
-data "template_file" "hulahoop_jump_server_policy" {
-  template = "${file("policies/hulahoop_jump_server.tpl")}"
-
-  vars = {
-    region = var.aws_region
-    account_id = data.aws_caller_identity.current.account_id
-    security_group_id = aws_security_group.hulahoop_jump_server.id
-  }
-}
-
 ### Caller
 
 resource "aws_iam_policy" "hulahoop_caller" {
   name        = "hulahoop_caller"
   path        = "/"
-  description = "Hulahoop policy to be attached to user for calling party" 
-  policy      = "${data.template_file.hulahoop_caller_policy.rendered}"
+  description = "Hulahoop policy to be attached to user for calling party"
+  policy = templatefile("policies/hulahoop_caller.tpl",
+    {
+      region            = var.aws_region,
+      account_id        = data.aws_caller_identity.current.account_id,
+      instance_role     = aws_iam_role.hulahoop_jump_server.name,
+      security_group_id = aws_security_group.hulahoop_jump_server.id
+    }
+  )
 }
 
 resource "aws_iam_user" "hulahoop_caller" {
@@ -83,15 +86,3 @@ resource "aws_iam_user_policy_attachment" "hulahoop_caller" {
   user       = aws_iam_user.hulahoop_caller.name
   policy_arn = aws_iam_policy.hulahoop_caller.arn
 }
-
-data "template_file" "hulahoop_caller_policy" {
-  template = "${file("policies/hulahoop_caller.tpl")}"
-
-  vars = {
-    region = var.aws_region
-    account_id = data.aws_caller_identity.current.account_id
-    instance_role = aws_iam_role.hulahoop_jump_server.name
-    security_group_id = aws_security_group.hulahoop_jump_server.id
-  }
-}
-
